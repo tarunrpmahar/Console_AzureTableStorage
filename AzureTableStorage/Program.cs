7ac23faf6f723c852a766c7d1f673e7c2220402d
tarunrpmahar <tsmahar1997@gmail.com>
2021-05-29 16:50:47 +0530

    Add project files.

---
+++ AzureTableStorage/Program.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureTableStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Table storage sample");
+
+            var storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=apithreelayerarchstorage;AccountKey=QERpGqXXMGMLOlG1zCHdJAeANoFpdSiTm7YRc+IhrDobHoD61S7HLZXx2SfXN/ASGoqskVjKLmIx5hzWFHLmAg==;EndpointSuffix=core.windows.net";
+            var tableName = "tsmtable";
+
+            CloudStorageAccount storageAccount;
+            storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+            CloudTable table = tableClient.GetTableReference(tableName);
+
+
+            CustomerEntity customer = new CustomerEntity("Tarun", "Singh Mahar")
+            {
+                Email = "tarun@gmail.com",
+                PhoneNumber = "+91-7777766666"
+            };
+
+
+            //CreateCustomer(table, customer).Wait();
+
+            //ReadCustomer(table, "Tarun", "Singh Mahar").Wait();
+
+            //UpdateCustomer(table, customer).Wait();
+
+            DeleteCustomer(table, "Tarun", "Singh Mahar").Wait();
+
+        }
+
+        public static async Task CreateCustomer(CloudTable table, CustomerEntity customer)
+        {
+            TableOperation createOperation = TableOperation.Insert(customer);
+
+            // Execute the operation.
+            TableResult result = await table.ExecuteAsync(createOperation);
+            CustomerEntity createdCustomer = result.Result as CustomerEntity;
+
+            Console.WriteLine("Created customer.");
+        }
+
+        public static async Task ReadCustomer(CloudTable table, string firstName, string lastName)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>(firstName, lastName);
+
+            TableResult result = await table.ExecuteAsync(retrieveOperation);
+            CustomerEntity customer = result.Result as CustomerEntity;
+
+            if (customer != null)
+            {
+                Console.WriteLine("Fetched \t{0}\t{1}\t{2}\t{3}",
+                    customer.PartitionKey, customer.RowKey, customer.Email, customer.PhoneNumber);
+            }
+        }
+
+        public static async Task UpdateCustomer(CloudTable table, CustomerEntity customer)
+        {
+            TableOperation updateOperation = TableOperation.InsertOrMerge(customer);
+
+            // Execute the operation.
+            TableResult result = await table.ExecuteAsync(updateOperation);
+            CustomerEntity updatedCustomer = result.Result as CustomerEntity;
+
+            Console.WriteLine("Updated user.");
+        }
+
+        public static async Task DeleteCustomer(CloudTable table, string firstName, string lastName)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>(firstName, lastName);
+            TableResult result = await table.ExecuteAsync(retrieveOperation);
+            CustomerEntity customer = result.Result as CustomerEntity;
+
+            TableBatchOperation batchOperation = new TableBatchOperation();
+            customer.ETag = "*";
+            batchOperation.Delete(customer);
+            table.ExecuteBatch(batchOperation);
+
+            Console.WriteLine("Deleted customer");
+        }
+    }
+
+    public class CustomerEntity : TableEntity
+    {
+        public CustomerEntity() { }
+        public CustomerEntity(string lastName, string firstName)
+        {
+            PartitionKey = lastName;
+            RowKey = firstName;
+        }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
+
---
+++ NewAzureTableStorage/Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+
+namespace NewAzureTableStorage
+{
+    class Program
+    {
+        public class EmployeeEntity : TableEntity
+        {
+            public EmployeeEntity() { }
+            public EmployeeEntity(string deptName, string empName)
+            {
+                PartitionKey = deptName;
+                RowKey = empName;
+            }
+
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Specialization { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Table storage sample");
+            
+            var storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=apithreelayerarchstorage;AccountKey=QERpGqXXMGMLOlG1zCHdJAeANoFpdSiTm7YRc+IhrDobHoD61S7HLZXx2SfXN/ASGoqskVjKLmIx5hzWFHLmAg==;EndpointSuffix=core.windows.net";           
+            var tableName = "tsmtable";
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient(); //(new TableClientConfiguration());
+            CloudTable tableEmployee = tableClient.GetTableReference(tableName);
+            tableEmployee.CreateIfNotExists();
+
+            EmployeeEntity emp1 = new EmployeeEntity("Training1", "Emp1")
+            {
+                Email = "emp1@gmail.com",
+                PhoneNumber = "+91-9607865228"
+            };
+            EmployeeEntity emp2 = new EmployeeEntity("Training1", "Emp2")
+            {
+                Email = "emp2@gmail.com",
+                PhoneNumber = "+91-7687056228"
+            };
+            EmployeeEntity emp3 = new EmployeeEntity("Development", "Emp1")
+            {
+                Email = "emp3@gmail.com",
+                PhoneNumber = "+91-7852385228",
+                Specialization = "Engineering"
+            };
+
+            TableBatchOperation batchOperation = new TableBatchOperation();
+
+            //insert
+            /*batchOperation.InsertOrReplace(emp1);
+            batchOperation.InsertOrReplace(emp2);*/
+
+            //batchOperation.InsertOrReplace(emp3);   //all batch operation run toghethet for same partition key so this will give error
+
+            /*tableEmployee.ExecuteBatch(batchOperation);*/
+
+            /*batchOperation = new TableBatchOperation();
+            batchOperation.InsertOrReplace(emp3);
+            tableEmployee.ExecuteBatch(batchOperation);*/
+
+            //read
+            /*TableOperation operation = TableOperation.Retrieve<EmployeeEntity>("Development", "Emp1");
+            TableResult result = tableEmployee.Execute(operation);
+            EmployeeEntity employee = result.Result as EmployeeEntity;
+            if (employee != null)
+            {
+                Console.WriteLine("Fetched \t{0}\t{1}\t{2}\t{3}",
+                    employee.PartitionKey, employee.RowKey, employee.Email, employee.PhoneNumber);
+            }*/
+
+            //delete
+            /*emp3.ETag = "*";
+            batchOperation.Delete(emp3);
+            tableEmployee.ExecuteBatch(batchOperation);*/
+
+            TableOperation operation = TableOperation.Retrieve<EmployeeEntity>("Tarun", "Singh Mahar");
+            TableResult result = tableEmployee.Execute(operation);
+            EmployeeEntity employee = result.Result as EmployeeEntity;
+            employee.ETag = "*";
+            batchOperation.Delete(employee);
+            tableEmployee.ExecuteBatch(batchOperation);
+
+        }
+    }
+}
